601c5918cb00de009fac712304de6878e79a10ea
Artur Kubiak <arturkubiak9@gmail.com>
2022-01-07 17:01:43 +0100

    Update project

--- src/main/java/com/java17/gatewaydb/web/GatewayDbController.java
+++ src/main/java/com/java17/gatewaydb/web/GatewayDbController.java
@@ -1,20 +1,23 @@
 package com.java17.gatewaydb.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
-@RequestMapping("/api/v1/test-db")
+@RequestMapping("/test")
 public class GatewayDbController {
 
     @GetMapping("/get-response")
-    public String getTesResponse() {
-        return "It's working";
+    public ResponseEntity<String> getTesResponse() {
+        return ok("It's working");
     }
 
     @GetMapping("get-admin-response")
-    public String getAdminResponse() {
-        return "You are administrator if you see this";
+    public ResponseEntity<String> getAdminResponse() {
+        return ok("You are administrator if you see this");
     }
 }
